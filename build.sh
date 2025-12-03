f4076cdd416dbb6d13ad71789dc2234e3c39a907
pipx2 <pipx2@proton.me>
2025-12-03 23:37:32 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ MAIN_PKG="./cmd/sanitizer/"
 
 # Output directory
 OUTPUT_DIR="./build"
-if [ -z "$OUTPUT_DIR" ]; then
+if [ -d "$OUTPUT_DIR" ]; then
+    echo "Removing old binaries in $OUTPUT_DIR"
     rm -rf "$OUTPUT_DIR"
 fi
 mkdir -p "$OUTPUT_DIR"
 
 echo "Building clipboard-sanitizer..."
 go build -o "$OUTPUT_DIR"/clipboard-sanitizer "$MAIN_PKG"
-echo "Binaries are found in $OUTPUT_DIR"
+echo "New binaries are found in $OUTPUT_DIR"
